9f1c1904d0c654575919c47447dfc426728ed21b
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2016-06-05 12:42:30 +0200

    Hide search box on ESC.

--- GistPaste.Desktop/MainWindow.xaml.cs
+++ GistPaste.Desktop/MainWindow.xaml.cs
@@ -34,5 +34,13 @@ namespace GistPaste.Desktop
         {
             this.Hide();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Hide();
+            }
+        }
     }
 }
