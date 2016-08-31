98e41fe5081385cfba9c2da39e22306f3073c7c4
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2016-08-31 20:02:04 +0200

    First unit test for LowLevelKeyboardHook. Yay!

---
+++ GistPaste.Desktop.UnitTests/WindowsHooks/LowLevelKeyboardHookTests.cs
@@ -0,0 +1,21 @@
+﻿using FakeItEasy;
+using GistPaste.Desktop.WindowsHooks;
+using Xunit;
+
+namespace GistPaste.Desktop.UnitTests.WindowsHooks
+{
+    public class LowLevelKeyboardHookTests
+    {
+        [Fact]
+        public void Constructor_DoesntCreateAHook()
+        {
+            var user32 = A.Fake<IUser32>();
+            var kernel32 = A.Fake<IKernel32>();
+
+            var sut = new LowLevelKeyboardHook(user32, kernel32);
+
+            A.CallTo(user32).MustNotHaveHappened();
+            A.CallTo(kernel32).MustNotHaveHappened();
+        }
+    }
+}
